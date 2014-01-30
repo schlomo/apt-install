dc1d403e8c2f39e5b94f7054f512cb97c9e058ce
Schlomo Schapiro <github@schlomo.schapiro.org>
2014-01-30 23:58:04 +0100

    Chaining the transactions in a single progress dialog does not work. This works now, but looks ugly.

--- README.md
+++ README.md
@@ -3,7 +3,9 @@ apt-install
 
 Update APT cache and install package(s) with progress GUI.
 
-I hacked it together in order to learn a bit about the aptdaemon. Python code is ugly but works. Needs more error handling.
+I hacked it together in order to learn a bit about the aptdaemon, GLib, asynchronous programming etc.
+
+Python code is ugly but works. Needs more error handling. Could probably be written much nicer.
 
 Usage:
 ```
--- apt-install.py
+++ apt-install.py
@@ -12,15 +12,13 @@ from aptdaemon.client import AptClient
 
 from aptdaemon.gtk3widgets import AptErrorDialog, AptConfirmDialog, AptProgressDialog
 import aptdaemon.errors
+from aptdaemon.enums import *
 
 
 loop = GLib.MainLoop()
+aptclient = AptClient()
+packages=[]
 
-def on_finished(trans, exit):
-    loop.quit()
-    if exit != "exit-success":
-        sys.exit(1)
-        
 def on_error(error):
     if isinstance(error, aptdaemon.errors.NotAuthorizedError):
         # Silently ignore auth failures
@@ -32,27 +30,45 @@ def on_error(error):
     dia.run()
     dia.hide()
 
-if __name__ == "__main__":
-    packages = sys.argv[1:]
-    if packages:
-        aptclient = AptClient()
-        try:
-            # Setting up transactions
-            trans_update = aptclient.update_cache()
-            trans_inst = aptclient.install_packages(packages)
-            # Trigger on last transaction finished
-            trans_inst.connect("finished", on_finished)
-            # Chaining transactions
-            trans_inst.run_after(trans_update)
-            dia = AptProgressDialog(trans_update)
-            dia.run(close_on_finished=True, show_error=True,
+def on_finished_update(trans, exit):
+    GLib.timeout_add(200,do_install)
+    return True
+
+def do_update():
+    trans_update = aptclient.update_cache()
+    trans_update.connect("finished",on_finished_update)
+    
+    dia = AptProgressDialog(trans_update)
+    dia.run(close_on_finished=True, show_error=True,
+            reply_handler=lambda: True,
+            error_handler=on_error,
+            )
+    return False
+
+
+def on_finished_install(trans, exit):
+    loop.quit()
+    if exit != "exit-success":
+        sys.exit(1)
+
+def do_install():
+    trans_inst = aptclient.install_packages(packages)
+    trans_inst.connect("finished", on_finished_install)
+    dia2 = AptProgressDialog(trans_inst)
+    dia2.run(close_on_finished=True, show_error=True,
                     reply_handler=lambda: True,
                     error_handler=on_error,
                     )
-        except Exception as error:
-            print error
-            loop.quit()
+    return False
+    
+
+if __name__ == "__main__":
+    packages = sys.argv[1:]
+    if packages:
+        GLib.timeout_add(50,do_update)
         loop.run()
     else:
-        print("Usage: %s <package> [...]\nLicensed under the GNU General Public License, see http://www.gnu.org/licenses/gpl.html\nWritten by Schlomo Schapiro" % sys.argv[0])
+        print("""Usage: %s <package> [...]
+Licensed under the GNU General Public License, see http://www.gnu.org/licenses/gpl.html
+Written by Schlomo Schapiro, see https://github.com/schlomo/apt-install""" % sys.argv[0])
         sys.exit(1)
